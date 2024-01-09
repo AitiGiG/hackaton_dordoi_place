2a9466291deb4c8725086d4c0b494df3a0b7d25c
FitStreet <fitstreety@gmail.com>
2024-01-09 18:32:47 +0600

    product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey(
+        'Category', 
+        on_delete=models.CASCADE,
+        related_name='products',
+    )
+    owner = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+        related_name='products',
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='product_images', blank=True)
+    stock = models.PositiveIntegerField(default=0)
+    avalible = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    upload_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if self.stock == 0:
+            self.available = False
+        super().save(*args, **kwargs)
+
+    
--- product/views.py
+++ product/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from .serializers import ProductSerializer
+from .models import Product
+from .permissions import IsOwnerOrAdmin
+
+class StandartResultPagination(PageNumberPagination):
+    page_size = 5
+    page_query_param= 'page'
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = PageNumberPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['title']
+    filersets_fields = ['category']
+
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PATCH', 'PUT', 'DELETE']:
+            return [permissions.IsAdminUser(), IsOwnerOrAdmin()]
+        return [permissions.AllowAny()]
+
+
+
+
